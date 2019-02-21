5bfe34eee8e5e432d1bedc89f1bc4c10ef30dd25
Clara Garcia <clara.garcia.pascual@accenture.com>
2019-02-21 01:47:02 +0100

    working on CoolButton Iteration 3

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import FormField from './components/FormField';
+import CoolButton from './components/CoolButton';
 
 export default class App extends Component {
   render() {
@@ -9,6 +10,8 @@ export default class App extends Component {
         <Navbar />
         <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
         <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+        <CoolButton isSmall isDanger className="is-rounded my-class">Button 1</CoolButton>
+        <CoolButton isSmall isSuccess>Button 2</CoolButton>
       </div>
     );
   }
